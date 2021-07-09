f813c2dbf23b744d169b43c1253691768e60e977
n-rdy <giuseppegelardi@icloud.com>
2021-07-09 12:46:08 -0500

    added template/skeleton functions for single search criteria. still need to add userinput

---
+++ notes.js
@@ -0,0 +1,125 @@
+//set up userinput!!!!
+
+//template
+// const searchSingleCriteria = (people) => {
+//   let selected = []
+//   people.forEach((person) => {
+//     if (person.{} === "") {
+//       selected.push(person)
+//     }
+//   })
+//   return selected
+// }
+// searchSingleCriteria(data)
+
+//set up userinput!!!!
+
+// const searchSingleCriteria = (people) => {
+//   let selected = []
+//   people.forEach((person) => {
+//     if (person.{} === "") {
+//       selected.push(person)
+//     }
+//   })
+//   return selected
+// }
+// searchSingleCriteria(data)
+
+// const searchSingleCriteriaFirstName = (people) => {
+//   let selected = []
+//   people.forEach((person) => {
+//     if (person.firstName === "Mattias") {
+//       selected.push(person)
+//     }
+//   })
+//   return selected
+// }
+// searchSingleCriteriaFirstName(data)
+
+// const searchSingleCriteriaLastName = (people) => {
+//   let selected = []
+//   people.forEach((person) => {
+//     if (person.lastName === "Bob") {
+//       selected.push(person)
+//     }
+//   })
+//   return selected
+// }
+// searchSingleCriteriaLastName(data)
+
+// const searchSingleCriteriaGender = (people) => {
+//   let selected = []
+//   people.forEach((person) => {
+//     if (person.gender === "male") {
+//       selected.push(person)
+//     }
+//   })
+//   return selected
+// }
+// searchSingleCriteriaGender(data)
+
+// const searchSingleCriteriaDOB = (people) => {
+//   let selected = []
+//   people.forEach((person) => {
+//     if (person.dob === "8/5/1967") {
+//       selected.push(person)
+//     }
+//   })
+//   return selected
+// }
+// searchSingleCriteriaDOB(data)
+
+// const searchSingleCriteriaHeight = (people) => {
+//   let selected = []
+//   people.forEach((person) => {
+//     if (person.height === 61) {
+//       selected.push(person)
+//     }
+//   })
+//   return selected
+// }
+// searchSingleCriteriaHeight(data)
+
+// const searchSingleCriteriaWeight = (people) => {
+//   let selected = []
+//   people.forEach((person) => {
+//     if (person.weight === 112) {
+//       selected.push(person)
+//     }
+//   })
+//   return selected
+// }
+// searchSingleCriteriaWeight(data)
+
+// const searchSingleCriteriaWeight = (people) => {
+//   let selected = []
+//   people.forEach((person) => {
+//     if (person.weight === 112) {
+//       selected.push(person)
+//     }
+//   })
+//   return selected
+// }
+// searchSingleCriteriaWeight(data)
+
+// const searchSingleCriteriaEyecolor = (people) => {
+//   let selected = []
+//   people.forEach((person) => {
+//     if (person.eyeColor === "blue") {
+//       selected.push(person)
+//     }
+//   })
+//   return selected
+// }
+// searchSingleCriteriaEyecolor(data)
+
+// const searchSingleCriteriaOccupation = (people) => {
+//   let selected = []
+//   people.forEach((person) => {
+//     if (person.occupation === "programmer") {
+//       selected.push(person)
+//     }
+//   })
+//   return selected
+// }
+// searchSingleCriteriaOccupation(data)
